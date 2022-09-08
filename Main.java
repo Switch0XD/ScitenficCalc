ad3951f300010b89be4c578bc805c820f2079520
Kuldeep Singh <switch2ks@gmail.com>
2022-09-08 14:42:48 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Number: ");
+    float num1 = sc.nextFloat();
+    System.out.print("Enter Number: ");
+    float num2 = sc.nextFloat();
+    System.out.println(num1 + num2);
+  }
+}
